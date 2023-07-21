c802deff1e9a84f382c5448de59013e5da5f2f1e
unknown <satya-1298>
2023-07-21 17:25:45 +0530

    [Satya]Add : Product Review management using Linq

---
+++ Lambda_LinQ_Demo/ProductReview.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda_LinQ_Demo
+{
+    //lambda expression is used to create anonymous function ,expression and body member
+    //It's a short block of code that accepts parameter and return a value
+    //Use => - is a lambda operator which is used for all the lambda expressions.
+
+    //LinQ- It's a language Integrated Query 
+    //It is used for filtering ,sorting ,grouping
+    //Mainly it is used for fetching the data from Xml document, database and collections
+    public class ProductReview
+    {
+        //ProductID, UserID, Rating, Review and isLike fields
+        public int ProductID { get; set; }
+        public int UserID { get; set; }
+        public int Rating { get; set; }
+        public string Review { get; set; }  
+        public bool IsLike { get; set; }
+    }
+}
